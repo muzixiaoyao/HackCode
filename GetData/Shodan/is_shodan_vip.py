a26e6f317f18b6c19d125666160782d144f3e94f
xcracker <wsxiaoyao@live.com>
2017-09-18 06:54:33 +0800

    2017.09.18

--- GetData/Shodan/is_shodan_vip.py
+++ GetData/Shodan/is_shodan_vip.py
@@ -1,9 +1,18 @@
+# -*- coding: UTF-8 -*- 
 import shodan
-SHODAN_API_KEY = "11111111111111111111111111111111"
-api = shodan.Shodan(SHODAN_API_KEY)
-try:
-        results = api.search("apache", page=20, limit=None, offset=None, facets=None, minify=True)
-        print "vip"
-except shodan.APIError, e:
-        print "穷逼"
+import sys
+def is_vip(key="123123123"):
+        SHODAN_API_KEY = key
+        api = shodan.Shodan(SHODAN_API_KEY)
+        try:
+                results = api.search("apache", page=20, limit=None, offset=None, facets=None, minify=True)
+                return True
+        except shodan.APIError, e:
+                return False
 
+if __name__ == '__main__':
+        KEY = sys.argv[1]
+        if is_vip(KEY):
+                print "土豪"
+        else:
+                print "屌丝"
